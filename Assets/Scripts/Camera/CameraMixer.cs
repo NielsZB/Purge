fb4b76db58d694014e667c304845be3281f5f73a
Niels <nzhansen@gmail.com>
2019-10-02 16:57:21 +0200

    Thing

---
+++ Assets/Scripts/Camera/CameraMixer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Rendering;
+
+public class CameraMixer : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCamera;
+    CinemachineVirtualCamera currentVirtualCamera;
+    Volume volume;
+    CinemachineBrain brain;
+    private void Start()
+    {
+        brain = Camera.main.GetComponent<CinemachineBrain>();
+        virtualCamera.enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        virtualCamera.enabled = true;
+        currentVirtualCamera = (CinemachineVirtualCamera)brain.ActiveVirtualCamera;
+        currentVirtualCamera.m_Priority = 5;
+        virtualCamera.m_Priority = 10;
+    }
+}
