965bc738105f5c38df3cd631fb2b75a676ca5ee3
Niels <nzhansen@gmail.com>
2019-10-14 15:27:08 +0200

    Cam evolved

---
+++ Assets/Scripts/Camera/CameraMixingManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[System.Serializable]
+public struct BlendCamera
+{
+    public CinemachineVirtualCameraBase camera;
+    public Transform originalParent;
+    public float weight;
+}
+
+public class CameraMixingManager : MonoBehaviour
+{
+    CinemachineMixingCamera mixingCamera;
+
+    public void AddCamera(BlendCamera blendCamera)
+    {
+        blendCamera.camera.transform.SetParent(transform);
+        var x = mixingCamera.ChildCameras;
+        mixingCamera.SetWeight(blendCamera.camera, blendCamera.weight);
+        blendCamera.camera.gameObject.SetActive(true);
+    }
+
+    public void Add(BlendCamera blendCamera)
+    {
+        blendCamera.camera.transform.SetParent(transform);
+        var x = mixingCamera.ChildCameras;
+        mixingCamera.SetWeight(blendCamera.camera, blendCamera.weight);
+        blendCamera.camera.gameObject.SetActive(true);
+    }
+
+    public void Add(BlendCamera[] blendCameras)
+    {
+        for (int i = 0; i < blendCameras.Length; i++)
+        {
+            blendCameras[i].camera.transform.SetParent(transform);
+        }
+        var x = mixingCamera.ChildCameras;
+        for (int i = 0; i < blendCameras.Length; i++)
+        {
+            mixingCamera.SetWeight(blendCameras[i].camera, blendCameras[i].weight);
+            blendCameras[i].camera.gameObject.SetActive(true);
+        }
+    }
+
+
+    public void Remove(BlendCamera blendCamera)
+    {
+        blendCamera.camera.transform.SetParent(blendCamera.originalParent);
+        blendCamera.camera.gameObject.SetActive(false);
+    }
+
+    public void Remove(BlendCamera[] blendCameras)
+    {
+        for (int i = 0; i < blendCameras.Length; i++)
+        {
+            BlendCamera blendCamera = blendCameras[i];
+
+            blendCamera.camera.transform.SetParent(blendCamera.originalParent);
+            blendCamera.camera.gameObject.SetActive(false);
+        }
+    }
+
+
+    public void UpdateWeight(BlendCamera blendCamera)
+    {
+        mixingCamera.SetWeight(blendCamera.camera, blendCamera.weight);
+    }
+
+    public void UpdateWeight(BlendCamera[] blendCameras)
+    {
+        for (int i = 0; i < blendCameras.Length; i++)
+        {
+            mixingCamera.SetWeight(blendCameras[i].camera, blendCameras[i].weight);
+        }
+    }
+
+    private void Start()
+    {
+        mixingCamera = GetComponent<CinemachineMixingCamera>();
+    }
+}
---
+++ Assets/Scripts/Utility/Editor/CameraMixingVolumeEditor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraMixingVolume))]
+public class CameraMixingVolumeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+
+    public void OnSceneGUI()
+    {
+        var mixingVolume = target as CameraMixingVolume;
+
+        if (mixingVolume.zones.Length > 0)
+        {
+            for (int i = 0; i < mixingVolume.zones.Length; i++)
+            {
+
+                CameraMixingVolume.Zone zone = mixingVolume.zones[i];
+
+
+
+                EditorGUI.BeginChangeCheck();
+                Vector3 position = Handles.PositionHandle(zone.position, Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(mixingVolume, "Change position");
+                    zone.position = position;
+                }
+                EditorGUI.BeginChangeCheck();
+                Handles.color = Color.grey;
+                float radius = Handles.RadiusHandle(Quaternion.identity, zone.position, zone.radius);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(mixingVolume, "Change radius");
+                    zone.radius = radius;
+                }
+                float outerRadius = zone.radius + zone.mixWidth;
+                EditorGUI.BeginChangeCheck();
+                Handles.color = Color.white;
+                float mixRadius = Handles.RadiusHandle(Quaternion.identity, zone.position, outerRadius);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(mixingVolume, "Change mixRadius");
+                    zone.mixWidth = mixRadius;
+                }
+
+                mixingVolume.zones[i] = zone;
+
+                GUIStyle centeredStyle = GUIStyle.none;
+                centeredStyle.alignment = TextAnchor.LowerCenter;
+
+                if (zone.camera != null)
+                {
+                    Handles.Label(zone.position, "Zone " + zone.camera.name, centeredStyle);
+                }
+                else
+                {
+
+                    Handles.Label(zone.position, "Zone " + i.ToString() + " camera needed!", centeredStyle);
+                }
+            }
+
+        }
+    }
+}
