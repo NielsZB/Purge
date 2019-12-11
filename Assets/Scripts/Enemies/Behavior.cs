af88b048188641fb5c5b17523e41719d83708447
Niels <nzhansen@gmail.com>
2019-12-11 11:28:48 +0100

    Movement

---
+++ Assets/Scripts/Enemies/Behavior.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Behavior : ScriptableObject
+{
+    public abstract Vector3 CalculatePreferredPosition();
+}
--- Assets/Scripts/Player/PAttack.cs
+++ Assets/Scripts/Player/PAttack.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class PAttack : MonoBehaviour
 {
-    
+    Animator animator;
+    Damager damager;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        damager = GetComponentInChildren<Damager>();
+    }
+
+    public void EnableDamage()
+    {
+        damager.CanDamage = true;
+    }
+
+    public void DisableDamage()
+    {
+        damager.CanDamage = false;
+    }
+    public void ResetAttack()
+    {
+        animator.SetBool("Attack", false);
+    }
     public void Melee()
     {
-
+        animator.SetBool("Attack", true);
     }
     public void MagicProjectile()
     {
