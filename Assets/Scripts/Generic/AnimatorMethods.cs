3b270e304dc9f6dd3cca9b97b1b7f81b6d164f12
Niels <nzhansen@gmail.com>
2020-01-09 13:57:46 +0100

    Recomitted

---
+++ Assets/Scripts/Generic/AnimatorMethods.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorMethods : MonoBehaviour
+{
+    [SerializeField] protected string parameter;
+    Animator animator;
+
+    protected void SetParameter(string parameter)
+    {
+        this.parameter = parameter;
+    }
+
+    protected void SetBool(bool value)
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        animator.SetBool(parameter, value);
+    }
+
+    protected void SetInt(int value)
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        animator.SetInteger(parameter, value);
+    }
+
+    protected void SetFloat(float value)
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        animator.SetFloat(parameter, value);
+    }
+
+    protected void SetTrigger()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        animator.SetTrigger(parameter);
+    }
+}
