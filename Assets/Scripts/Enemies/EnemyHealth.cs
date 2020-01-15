eee17171892e2bbeba5fe00459066062a62ce52a
Niels <nzhansen@gmail.com>
2020-01-15 01:41:10 +0100

    More Enemy and Violet

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float health;
+
+    public float currentHealth { get; private set; }
+    public void TakeDamage(float amount)
+    {
+
+    }
+}
