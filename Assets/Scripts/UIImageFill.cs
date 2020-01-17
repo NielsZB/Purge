84f2fccdf27bdfdc5ec8a34ef8c50a5934b06fce
Niels <nzhansen@gmail.com>
2020-01-17 19:26:07 +0100

    Updated UI Added stuff

---
+++ Assets/Scripts/UIImageFill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIImageFill : MonoBehaviour
+{
+    [SerializeField] FloatVariable value;
+
+    float currentValue;
+
+    Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        currentValue = value.Value;
+        image.fillAmount = currentValue;
+    }
+
+    private void Update()
+    {
+        if (currentValue != value.Value)
+        {
+            currentValue = value.Value;
+            image.fillAmount = currentValue;
+        }
+    }
+}
